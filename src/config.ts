50bc8e6cbe0cda9e47e8738d3d5edc64b0ab173c
gusarovf <6527375@mail.ru>
2020-08-04 22:26:37 +0300

    Replaced dbConn credentials with connection string.

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,7 @@ export const cfg = {
 }
 
 export const dbConn = {
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_DATABASE || "postgres",
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgres://postgres:postgres@localhost:5432/postgres",
 }
