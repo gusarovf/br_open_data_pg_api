9bad268a025feb2a8589e007c66aff7d2e597848
gusarovf <6527375@mail.ru>
2020-08-11 22:05:48 +0300

    Modified server indes. Now res is sending 404 status instead of empty array when nothing is found.

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,28 @@ import { getDocument } from "../database/actions/selects/selects_v1"
 import { getDocument as getDocumentV2 } from "../database/actions/selects/selects_v2"
 import { getLatestUpdatePostfix } from "../database"
 import { cfg } from "../config"
-import express from "express"
+import express, { Response as ExpressRes } from "express"
 import { getDbSize } from "../database/actions/selects"
+
+const sendResponse = (res: ExpressRes, success: boolean, data: any[]): void => {
+  res.type("application/json")
+  if (success) {
+    res.status(200)
+    res.send(JSON.stringify(data))
+  } else {
+    res.status(404)
+    res.sendStatus(404)
+  }
+}
 
 export const startServer = () => {
   const app = express()
   const port = cfg.serverPort
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    next()
+  })
 
   app.get("/", function (request, response) {
     const userAgent = request.headers["user-agent"]
@@ -18,46 +34,35 @@ export const startServer = () => {
     }
   })
 
-  app.get("/documents/:inn", async (request, response) => {
+  app.get("/documents/:inn", async (req, res) => {
     // /documents/860500102219
-    const inn = request.params.inn as string
-    if (!inn) {
-      response.send("Error. Please specify inn.")
-    } else {
-      const postfix = (await getLatestUpdatePostfix()) || ""
-      const fields = await getDocument(inn, postfix)
-      response.type("application/json")
-      fields.success ? response.status(200) : response.status(404)
+    const inn = req.params.inn
+    if (!inn) res.sendStatus(404)
 
-      response.send(fields.success ? JSON.stringify(fields.data) : [])
-    }
+    const postfix = (await getLatestUpdatePostfix()) || ""
+    const fields = await getDocument(inn, postfix)
+    sendResponse(res, fields.success, fields.data)
   })
 
-  app.get("/v2/documents/:inn", async (request, response) => {
+  app.get("/v2/documents/:inn", async (req, res) => {
     // /v2/documents/143523123723,143523630498
-    const inn = request.params.inn as string
+    const inn = req.params.inn
     if (!inn) {
-      response.send("Error. Please specify inn.")
+      res.sendStatus(404)
     } else {
       const inns = inn.includes(",")
-        ? inn.split(",").map((inn: string) => inn.replace(/\D+/g, ""))
+        ? inn.split(",").map((inn) => inn.replace(/\D+/g, ""))
         : [inn.replace(/\D+/g, "")]
 
       const postfix = (await getLatestUpdatePostfix()) || ""
       const fields = await getDocumentV2(inns, postfix)
-      response.type("application/json")
-      fields.success ? response.status(200) : response.status(404)
-
-      response.send(fields.success ? JSON.stringify(fields.data) : [])
+      sendResponse(res, fields.success, fields.data)
     }
   })
 
   app.get("/dbsize", async (request, response) => {
     const fields = await getDbSize()
-    response.type("application/json")
-    fields.success ? response.status(200) : response.status(404)
-
-    response.send(fields.success ? JSON.stringify(fields.data) : [])
+    sendResponse(response, fields.success, [fields.data])
   })
 
   app.listen(port, () => console.log(`App is listening on port ${port}!`))
